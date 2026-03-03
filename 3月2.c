cacf2465b102310c6b427244b1a83aaacd971c04
zzzz1030 <3763341008@qq.com>
2026-03-03 11:20:32 +0800

    Add files via upload

---
+++ 3月2.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+
+
+
+
+
+
+
+//int main()
+//{
+//	int price = 0;
+//
+//	printf("请输入金额（元）");
+//	scanf("%d", &price);
+//
+//	int change = 100 - price;
+//
+//	printf("找您%d元\n", change);
+//
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	printf("%d", 35 + 6 * 2 / 3);
+//	return 0;
+//}
