ba68eb80f7f2030a2e1525d91b20fa364f152fad
zzzz1030 <3763341008@qq.com>
2026-03-04 16:53:34 +0800

    3.3 Chapter 2

---
+++ 2026.3.3.c
@@ -0,0 +1,63 @@
+#include <stdio.h>
+
+
+//int main()
+//{
+//	int a;
+//	a = 10;
+//
+//	printf("a++=%d\n", a++);
+//	printf("a=%d\n", a);
+//
+//	printf("++a=%d\n", ++a);
+//	printf("a=%d\n", a);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	int a = 5;
+//	int b = 6;
+//	int c;
+//
+//	c = a;
+//	a = b;
+//	b = c;
+//
+//	printf("a=%d,b=%d", a, b);
+//	return 0;
+//}
+
+//int main()
+//{
+//	int a, b;
+//
+//	scanf_s("%d %d", & a, &b);
+//
+//	double c = (a + b) / 2.0;
+//
+//	printf("%d和%d的平均值=%f\n", a, b, c);
+//
+//	return 0;
+//}
+
+
+
+//int main()
+//{
+//	int hour1, minute1;
+//	int hour2, minute2;
+//
+//	scanf_s("%d %d", &hour1, &minute1);
+//	scanf_s("%d %d", &hour2, &minute2);
+//
+//	int t1 = hour1 * 60 + minute1;
+//	int t2 = hour2 * 60 + minute2;
+//
+//	int t = t2 - t1;
+//
+//	printf("时间差是%d小时%d分钟", t / 60, t % 60);
+//
+//	return 0;
+//}
